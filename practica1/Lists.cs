2278a710e1e966a99d4d54c781da9dc4f7c2c7d5
Simon <simonchumacero26@gmail.com>
2024-05-20 18:34:10 -0400

    Agregar archivos de proyecto.

---
+++ practica1/Lists.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica1
+{
+    internal class Lists
+    {
+        static void Main()
+        {
+            Console.WriteLine("prueba clase listas");
+        }
+    }
+}
---
+++ practica1/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+// Lists
+List<int> numbers = new List<int>();
+
+for (int i = 0; i < 10; i++)
+{
+    numbers.Add(i);
+}
+
+Console.WriteLine("Elements of the list: ");
+foreach (int number in numbers)
+{
+    Console.WriteLine(number);
+}
+
+numbers.RemoveAt(0);
+numbers.RemoveAt(numbers.Count - 1);
+
+Console.WriteLine("Updated List");
+foreach (int number in numbers)
+{
+    Console.WriteLine(number);
+}
+
+
+// Dictionaries
+Dictionary<string, int> studentsInfo = new Dictionary<string, int>()
+{
+    { "Simon", 70},
+    { "Santiago", 70 },
+    { "Nicolas", 70 },
+    { "Araceli", 70 },
+    { "Diego", 70 },
+};
+
+foreach( var student in  studentsInfo)
+{
+    Console.WriteLine( $"{student.Key}: {student.Value}");
+}
